154f3b80995806e1c1bec9ad2ca226fcb43d8f1c
jjwikner <jjwikner@gmail.com>
2018-11-11 17:08:20 +0100

    and to receive messages

---
+++ rcvMessage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import os
+from subprocess import call
+import datetime
+
+print("Hej!")
+
+fileName = "./dragino_lora_app"
+
+message = "abcdefghijklmnopqrstuwvxyz0123456789";
+message = datetime.datetime.now().isoformat()
+
+call([fileName, "rec"])
+
+
