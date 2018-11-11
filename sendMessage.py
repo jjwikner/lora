758b383945825cdf9873c2f181dbf156f217406a
jjwikner <jjwikner@gmail.com>
2018-11-11 16:31:16 +0100

    f1rst

---
+++ sendMessage.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import os
+from subprocess import call
+import datetime
+
+print("Hej!")
+
+fileName = "./dragino_lora_app"
+
+message = "abcdefghijklmnopqrstuwvxyz0123456789";
+message = datetime.datetime.now().isoformat()
+
+call([fileName, "sender", message])
