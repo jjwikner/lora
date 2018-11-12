326b1038945cc36c6f2ed3042c7a808af75c72dc
jjwikner <jjwikner@gmail.com>
2018-11-12 10:16:13 +0100

    updated to cat ch frequency

--- rcvMessage.py
+++ rcvMessage.py
@@ -10,7 +10,12 @@ fileName = "./dragino_lora_app"
 
 message = "abcdefghijklmnopqrstuwvxyz0123456789";
 message = datetime.datetime.now().isoformat()
+freq1 = 868100000;
+freq2 = 868200000;
 
-call([fileName, "rec"])
+freqs = [freq1, freq2]
+
+for freq in freqs:
+    call([fileName, "rec", str(freq), "single"])
 
 
