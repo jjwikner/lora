607df220b265d5229091bfbd50f6e21d904518d6
jjwikner <jjwikner@gmail.com>
2018-11-12 14:21:40 +0100

    fix

--- protocol.py
+++ protocol.py
@@ -21,21 +21,30 @@ freq2 = 868200000;
 
 freqs = [freq1, freq2]
 
+# Subfunctions here
+
+
 ctr = 0;
+
 while ctr < 10:
     
     for freq in freqs:
         # Send the messages
         print('Sending at {}'.format(freq))
-        sendResults = subprocess.run([executable, "sender", str(freq), "NODE ID: "+ mac ], stdout=subprocess.PIPE)
+        sendResults = subprocess.run([executable, "sender", str(freq), "NODE ID:"+ mac ], stdout=subprocess.PIPE)
         # print(sendResults.stdout)
 
     for freq in freqs:
-        # Listen for response
+        # Listen for the response, if an ack is sent back, then that's the shit
         rcvResults = subprocess.run([executable, "rec", str(freq), "single"], stdout=subprocess.PIPE)        
         if "Payload:" in rcvResults.stdout.decode("utf-8"): 
             print("Someone did something");
             print(rcvResults.stdout)
+            if "NODE ID:" in rcvResults.stdout.decode("utf-8"): # Check here if message is for you.
+                print("Contact with master")
+
+    # Based on contact
+    
             
     ctr = ctr+1
 
